389645606656f938eaff5423f9f3794c0cc17df4
safmode <shiqinnizal@gmail.com>
2026-05-27 20:46:43 +0800

    Axios

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,9 +1,26 @@
 import axios from 'axios';
 
-// Set the base URL to the current origin (will be HTTPS in production)
+// Set base URL to current origin
 window.axios = axios;
 window.axios.defaults.baseURL = window.location.origin;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// Interceptor to force HTTPS for ALL requests
+window.axios.interceptors.request.use(config => {
+    if (config.url && typeof config.url === 'string') {
+        // If it's an absolute HTTP URL, convert to HTTPS
+        if (config.url.startsWith('http://')) {
+            config.url = config.url.replace('http://', 'https://');
+        }
+        // If it's a relative URL, ensure it uses the current origin (which is HTTPS)
+        if (config.url.startsWith('/')) {
+            config.baseURL = window.location.origin;
+        }
+    }
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
 
 if (typeof window !== 'undefined') {
     import('./echo');
