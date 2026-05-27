600622576ee7dc3b0e4a69ef65294f67b829682e
safmode <shiqinnizal@gmail.com>
2026-05-27 20:55:42 +0800

    Add global HTTPS interceptor to force all HTTP requests to HTTPS

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,27 +1,42 @@
 import axios from 'axios';
 
-// Set base URL to current origin
+// FORCE HTTPS - Fix for Render deployment
+const forceHTTPS = (url) => {
+    if (typeof url === 'string' && url.startsWith('http://')) {
+        return url.replace('http://', 'https://');
+    }
+    return url;
+};
+
+// Configure axios
 window.axios = axios;
 window.axios.defaults.baseURL = window.location.origin;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-// Interceptor to force HTTPS for ALL requests
+// Intercept all axios requests to force HTTPS
 window.axios.interceptors.request.use(config => {
-    if (config.url && typeof config.url === 'string') {
-        // If it's an absolute HTTP URL, convert to HTTPS
-        if (config.url.startsWith('http://')) {
-            config.url = config.url.replace('http://', 'https://');
-        }
-        // If it's a relative URL, ensure it uses the current origin (which is HTTPS)
-        if (config.url.startsWith('/')) {
-            config.baseURL = window.location.origin;
-        }
+    if (config.url) {
+        config.url = forceHTTPS(config.url);
+    }
+    if (config.baseURL) {
+        config.baseURL = forceHTTPS(config.baseURL);
     }
     return config;
-}, error => {
-    return Promise.reject(error);
 });
 
+// Intercept all fetch requests to force HTTPS
+const originalFetch = window.fetch;
+window.fetch = function(...args) {
+    let url = args[0];
+    if (typeof url === 'string') {
+        args[0] = forceHTTPS(url);
+    }
+    return originalFetch.apply(this, args);
+};
+
+// Also fix any links or router calls
 if (typeof window !== 'undefined') {
     import('./echo');
 }
+
+console.log('🔒 HTTPS interceptor active - all HTTP requests will be upgraded to HTTPS');
