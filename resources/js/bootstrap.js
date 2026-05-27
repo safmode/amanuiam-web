2195518c79c3f9cbb4a2b0c4e270010006978207
safmode <shiqinnizal@gmail.com>
2026-05-27 20:34:35 +0800

    Axios

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
-window.axios = axios;
 
+// Set the base URL to the current origin (will be HTTPS in production)
+window.axios = axios;
+window.axios.defaults.baseURL = window.location.origin;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 if (typeof window !== 'undefined') {
