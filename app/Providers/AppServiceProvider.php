952f9408ebd6869d1e4550f0631f836de8106238
safmode <shiqinnizal@gmail.com>
2026-05-27 22:53:03 +0800

    Force HTTPS redirects

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,11 +1,12 @@
 <?php
 
 namespace App\Providers;
+
 use App\Models\Report;
 use App\Observers\ReportObserver;
-
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;  // ADD THIS LINE
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +25,9 @@ class AppServiceProvider extends ServiceProvider
     {
         Vite::prefetch(concurrency: 3);
         Report::observe(ReportObserver::class);
+
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
