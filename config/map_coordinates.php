c6a11e456a681f74202615af20c2d5d7cfef9c1e
safmode <shiqinnizal@gmail.com>
2026-06-04 02:56:29 +0800

    change

--- config/map_coordinates.php
+++ config/map_coordinates.php
@@ -1,172 +1,172 @@
 <?php
 
 return array (
-  'Mahallah Asiah' => 
+  'Mahallah Asiah' =>
+  array (
+    'lat' => 3.2582577,
+    'lng' => 101.7333511,
+  ),
+  'Mahallah Aminah' =>
+  array (
+    'lat' => 3.2565195,
+    'lng' => 101.7318572,
+  ),
+  'Mahallah Safiyyah' =>
+  array (
+    'lat' => 3.2482136,
+    'lng' => 101.7340588,
+  ),
+  'Mahallah Maryam' =>
+  array (
+    'lat' => 3.247642,
+    'lng' => 101.7377777,
+  ),
+  'Mahallah Ruqayyah' =>
   array (
     'lat' => 3.2503254,
     'lng' => 101.7345347,
   ),
-  'Mahallah Aminah' => 
+  'Mahallah Ali' =>
+  array (
+    'lat' => 3.247642,
+    'lng' => 101.7377777,
+  ),
+  'Mahallah Faruq' =>
+  array (
+    'lat' => 3.248576,
+    'lng' => 101.7400305,
+  ),
+  'Mahallah Bilal' =>
+  array (
+    'lat' => 3.247642,
+    'lng' => 101.7377777,
+  ),
+  'Mahallah Asma' =>
+  array (
+    'lat' => 3.2561363,
+    'lng' => 101.7334729,
+  ),
+  'Mahallah Hafsah' =>
   array (
     'lat' => 3.2503254,
     'lng' => 101.7345347,
   ),
-  'Mahallah Safiyyah' => 
+  'Mahallah Halimah' =>
+  array (
+    'lat' => 3.2586235,
+    'lng' => 101.7342165,
+  ),
+  'Mahallah Siddiq' =>
+  array (
+    'lat' => 3.2477493,
+    'lng' => 101.7385177,
+  ),
+  'Mahallah Salahuddin' =>
+  array (
+    'lat' => 3.2570275,
+    'lng' => 101.7385388,
+  ),
+  'Mahallah Uthman' =>
+  array (
+    'lat' => 3.2491575,
+    'lng' => 101.7405593,
+  ),
+  'Mahallah Nusaibah' =>
   array (
     'lat' => 3.2503254,
     'lng' => 101.7345347,
   ),
-  'Mahallah Maryam' => 
+  'Mahallah Zubair Al-Awwam' =>
   array (
-    'lat' => 3.2503254,
-    'lng' => 101.7345347,
+    'lat' => 3.2465761,
+    'lng' => 101.7359093,
   ),
-  'Mahallah Ruqayyah' => 
+  'Mahallah Sumayyah' =>
   array (
-    'lat' => 3.2503254,
-    'lng' => 101.7345347,
+    'lat' => 3.2555813,
+    'lng' => 101.7393246,
   ),
-  'Mahallah Ali' => 
-  array (
-    'lat' => 3.2503254,
-    'lng' => 101.7345347,
-  ),
-  'Mahallah Faruq' => 
-  array (
-    'lat' => 3.2503254,
-    'lng' => 101.7345347,
-  ),
-  'Mahallah Bilal' => 
-  array (
-    'lat' => 3.2503254,
-    'lng' => 101.7345347,
-  ),
-  'Mahallah Asma' => 
-  array (
-    'lat' => 3.2503254,
-    'lng' => 101.7345347,
-  ),
-  'Mahallah Hafsah' => 
-  array (
-    'lat' => 3.2503254,
-    'lng' => 101.7345347,
-  ),
-  'Mahallah Halimah' => 
-  array (
-    'lat' => 3.2503254,
-    'lng' => 101.7345347,
-  ),
-  'Mahallah Siddiq' => 
-  array (
-    'lat' => 3.2503254,
-    'lng' => 101.7345347,
-  ),
-  'Mahallah Salahuddin' => 
-  array (
-    'lat' => 3.2503254,
-    'lng' => 101.7345347,
-  ),
-  'Mahallah Uthman' => 
-  array (
-    'lat' => 3.2503254,
-    'lng' => 101.7345347,
-  ),
-  'Mahallah Nusaibah' => 
-  array (
-    'lat' => 3.2503254,
-    'lng' => 101.7345347,
-  ),
-  'Mahallah Zubair Al-Awwam' => 
-  array (
-    'lat' => 3.2503254,
-    'lng' => 101.7345347,
-  ),
-  'Mahallah Sumayyah' => 
-  array (
-    'lat' => 3.2503254,
-    'lng' => 101.7345347,
-  ),
-  'KIRKHS (AHAS KIRKHS)' => 
+  'KIRKHS (AHAS KIRKHS)' =>
   array (
     'lat' => 3.2525287,
     'lng' => 101.7351951,
   ),
-  'KICT (ICT)' => 
+  'KICT (ICT)' =>
   array (
     'lat' => 3.2538595,
     'lng' => 101.7299568,
   ),
-  'KOE (Engineering)' => 
+  'KOE (Engineering)' =>
   array (
     'lat' => 3.2524313,
     'lng' => 101.7317276,
   ),
-  'KAED (Architecture)' => 
+  'KAED (Architecture)' =>
   array (
     'lat' => 3.2512812,
     'lng' => 101.7314632,
   ),
-  'KENMS (Economics)' => 
+  'KENMS (Economics)' =>
   array (
-    'lat' => 3.24991,
-    'lng' => 101.7379458,
+    'lat' => 3.2503254,
+    'lng' => 101.7345347,
   ),
-  'AIKOL (Law)' => 
+  'AIKOL (Law)' =>
   array (
     'lat' => 3.2513831,
     'lng' => 101.7378795,
   ),
-  'KOED (Education)' => 
+  'KOED (Education)' =>
   array (
     'lat' => 3.2533137,
     'lng' => 101.7340636,
   ),
-  'Dar al-Hikmah Library' => 
+  'Dar al-Hikmah Library' =>
   array (
     'lat' => 3.2515985,
     'lng' => 101.736551,
   ),
-  'Female Sports Complex' => 
+  'Female Sports Complex' =>
   array (
     'lat' => 3.2551677,
     'lng' => 101.7354885,
   ),
-  'Saidina Hamzah Stadium' => 
+  'Saidina Hamzah Stadium' =>
   array (
     'lat' => 3.2508492,
     'lng' => 101.7400161,
   ),
-  'IIUM Archery Range' => 
+  'IIUM Archery Range' =>
   array (
     'lat' => 3.2487308,
     'lng' => 101.7418354,
   ),
-  'UIA Football Turf' => 
+  'UIA Football Turf' =>
   array (
     'lat' => 3.249012,
     'lng' => 101.7436673,
   ),
-  'IIUM Cricket Ground' => 
+  'IIUM Cricket Ground' =>
   array (
     'lat' => 3.2513979,
     'lng' => 101.7425967,
   ),
-  'IIUM Rugby Field' => 
+  'IIUM Rugby Field' =>
   array (
     'lat' => 3.2534435,
     'lng' => 101.7430567,
   ),
-  'Padang Kawad UIAM' => 
+  'Padang Kawad UIAM' =>
   array (
-    'lat' => 3.1572924,
-    'lng' => 101.5547356,
+    'lat' => 3.2555412,
+    'lng' => 101.7409924,
   ),
-  'IIUM Educare' => 
+  'IIUM Educare' =>
   array (
     'lat' => 3.2562124,
     'lng' => 101.7403135,
   ),
-  'Sultan Haji Ahmad Shah Mosque IIUM' => 
+  'Sultan Haji Ahmad Shah Mosque IIUM' =>
   array (
     'lat' => 3.2512358,
     'lng' => 101.7350865,
